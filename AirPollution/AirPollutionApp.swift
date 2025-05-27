ff5229922cba8815f53fce367dc20680a127cdea
Mariam Babutsidze <mbabutsidze@devexperts.com>
2025-05-27 14:33:50 +0400

    Initial Commit

---
+++ AirPollution/AirPollutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirPollutionApp.swift
+//  AirPollution
+//
+//  Created by Mariam Babutsidze on 27.05.25.
+//
+
+import SwiftUI
+
+@main
+struct AirPollutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirPollutionTests/AirPollutionTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirPollutionTests.swift
+//  AirPollutionTests
+//
+//  Created by Mariam Babutsidze on 27.05.25.
+//
+
+import Testing
+@testable import AirPollution
+
+struct AirPollutionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
